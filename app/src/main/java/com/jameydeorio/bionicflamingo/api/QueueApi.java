3a180acfadb95715316b205f98924d790907de1f
Subathra Thanabalan <subathra.thanabalan@metmuseum.org>
2016-03-10 13:24:24 -0500

    Adding getQueueItem method to QueueApi

--- app/src/main/java/com/jameydeorio/bionicflamingo/api/QueueApi.java
+++ app/src/main/java/com/jameydeorio/bionicflamingo/api/QueueApi.java
@@ -1,18 +1,24 @@
 package com.jameydeorio.bionicflamingo.api;
 
+import com.jameydeorio.bionicflamingo.models.Book;
 import com.jameydeorio.bionicflamingo.models.QueueItem;
 
 import java.util.List;
+import java.util.Queue;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface QueueApi {
     @GET("queue/")
     Call<List<QueueItem>> getQueue();
+
+    @GET("queue/{id}/")
+    Call<QueueItem> getQueueItem(@Path("id") int id);
 
     @POST("queue/")
     Call<QueueItem> addToQueue(@Body QueueItem queueItem);
