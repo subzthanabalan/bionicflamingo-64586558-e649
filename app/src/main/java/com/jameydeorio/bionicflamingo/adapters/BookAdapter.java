2c1473b304f6e9e3b3d86852b3a1a695a20f53c8
Subathra Thanabalan <subathra.thanabalan@metmuseum.org>
2016-03-10 02:03:14 -0500

    Using Picasso to show images of book covers in both list fragments

--- app/src/main/java/com/jameydeorio/bionicflamingo/adapters/BookAdapter.java
+++ app/src/main/java/com/jameydeorio/bionicflamingo/adapters/BookAdapter.java
@@ -1,13 +1,16 @@
 package com.jameydeorio.bionicflamingo.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jameydeorio.bionicflamingo.R;
 import com.jameydeorio.bionicflamingo.models.Book;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -26,7 +29,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
     @Override
     public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.book_item, parent, false);
-        return new BookViewHolder(view, mOnClickListener);
+        return new BookViewHolder(view, mOnClickListener, parent.getContext());
     }
 
     @Override
@@ -41,18 +44,27 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
 
     public class BookViewHolder extends RecyclerView.ViewHolder {
         public TextView bookTitleLabel;
+        public ImageView bookImageView;
+        private Context mContext;
 
-        public BookViewHolder(View itemView, View.OnClickListener onClickListener) {
+        public BookViewHolder(View itemView, View.OnClickListener onClickListener, Context context) {
             super(itemView);
+            mContext = context;
+
             bookTitleLabel = (TextView) itemView.findViewById(R.id.bookTitleLabel);
             if (onClickListener != null) {
                 itemView.setOnClickListener(onClickListener);
             }
+
+            bookImageView = (ImageView) itemView.findViewById(R.id.bookImageView);
         }
 
         public void bindBook(Book book) {
             bookTitleLabel.setText(book.getTitle());
             itemView.setTag(book.getIdentifier());
+            Picasso.with(mContext)
+                    .load(book.getCoverUrl())
+                    .into(bookImageView);
         }
     }
 }
