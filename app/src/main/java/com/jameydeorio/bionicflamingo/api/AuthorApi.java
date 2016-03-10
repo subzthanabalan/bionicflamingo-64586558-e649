6b65b08759964810264b1604a9de213d5e0cb761
Subathra Thanabalan <subathra.thanabalan@metmuseum.org>
2016-03-10 13:45:34 -0500

    Fleshing out Author object and AuthorApi

--- app/src/main/java/com/jameydeorio/bionicflamingo/api/AuthorApi.java
+++ app/src/main/java/com/jameydeorio/bionicflamingo/api/AuthorApi.java
@@ -1,5 +1,18 @@
 package com.jameydeorio.bionicflamingo.api;
 
-public class AuthorApi {
-    // TODO
+import com.jameydeorio.bionicflamingo.models.Author;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface AuthorApi {
+    @GET("authors/")
+    Call<List<Author>> listAuthors();
+
+    @GET("authors/{id}/")
+    Call<Author> getAuthor(@Path("id") int id);
 }
+
